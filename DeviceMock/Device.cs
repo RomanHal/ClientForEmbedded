6271f67a4d107e35f9de6f0d51ae4bdb5b785eb9
RomanHal <romanhalacinski@gmail.com>
2021-01-28 23:07:03 +0100

    Fix and refactor DeviceMock

--- DeviceMock/Device.cs
+++ DeviceMock/Device.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO.Ports;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Timers;
 
@@ -10,30 +11,33 @@ namespace DeviceMock
 {
     class Device
     {
-        byte[] setting;
-        byte status;
+        byte[] _setting;
+        byte _status;
         SerialPort _port;
         Queue<byte> _queue = new Queue<byte>();
-        Timer _timer;
-        Timer _timeBetween;
+        System.Timers.Timer _timer;
+        System.Timers.Timer _timeBetween;
+        private uint _amount;
 
         public Device()
         {
             _port = new SerialPort("COM3");
             _port.DataReceived += _port_DataReceived;
             _port.Open();
-            _timer = new Timer(1000);
+            _timer = new System.Timers.Timer(1000);
             _timer.Elapsed += Timer_Elapsed;
             _timer.Start();
-            _timeBetween = new Timer();
+            _timeBetween = new System.Timers.Timer(1000);
             _timeBetween.Elapsed += _timeBetween_Elapsed;
-            setting = new byte[] { 0x00, 0x01, 0x02, 0x03 };
-            status = 0x01;
+            _setting = new byte[] { 0x00, 0x01, 0x02, 0x03 };
+            _status = 0x00;
         }
 
         private void _timeBetween_Elapsed(object sender, ElapsedEventArgs e)
         {
             SendShoot();
+            _amount--;
+            Console.WriteLine("Amount to do "+_amount);
         }
 
         private void SendShoot()
@@ -58,15 +62,16 @@ namespace DeviceMock
         private void _port_DataReceived(object sender, SerialDataReceivedEventArgs e)
         {
             var bytes = new byte[_port.BytesToRead];
-            _port.Read(bytes, 0, _port.BytesToRead);
+            _port.Read(bytes, 0, bytes.Length);
             bytes.ToList().ForEach(b => _queue.Enqueue(b));
         }
         public bool IsData()
         {
-            return _queue.Count > 0 ? true : false;
+            return _queue.Count > 0;
         }
         public byte GetData()
         {
+            while (!IsData()) Thread.Sleep(500);
             return _queue.Dequeue();
         }
         public void HandleCommunication()
@@ -105,6 +110,7 @@ namespace DeviceMock
             _timeBetween.Stop();
             Console.WriteLine("received Stop");
             Send(0x14);
+            _status = 3;
         }
 
         private void StartHandle()
@@ -112,23 +118,29 @@ namespace DeviceMock
             Console.WriteLine("received Start");
             _timeBetween.Start();
             Send(0x13);
+            _status = 2;
         }
 
         private void SetSettingsHandle()
         {
-            setting[0] = _queue.Dequeue();
-            setting[1] = _queue.Dequeue();
-            setting[2] = _queue.Dequeue();
-            setting[3] = _queue.Dequeue();
-            Console.WriteLine("received setSetting");
+            _setting[0] = _queue.Dequeue();
+            _setting[1] = _queue.Dequeue();
+            _setting[2] = _queue.Dequeue();
+            _setting[3] = _queue.Dequeue();
+            _amount = (uint)(_setting[1] << 8) + (uint)_setting[0];
+            Console.WriteLine("received setSetting, amount: "+_amount);
             Send(0x32);
+            _status = 1;
         }
 
         private void GetSettingsHandle()
         {
             byte[] response = new byte[5];
             response[0] = 0x12;
-            setting.CopyTo(response, 1);
+            _setting[0] = (byte)_amount;
+            _setting[1] = (byte)(_amount>>8);
+
+            _setting.CopyTo(response, 1);
             Console.WriteLine("received getSetting");
             Send(response);
         }
@@ -136,7 +148,8 @@ namespace DeviceMock
         private void GetStatusHandle()
         {
             Console.WriteLine("received getStatus");
-            Send(new byte[] {0x11,0x44 });
+
+            Send(new byte[] {0x11,_status });
         }
 
         private void HelloHandle()
